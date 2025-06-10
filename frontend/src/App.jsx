3310a3ee09050779cd66c7094634608758fc36f4
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-09 21:07:42 -0700

    router dom setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Navbar from "./components/navbar";
+import { Route, Routes } from "react-router-dom";
+import Login  from "./pages/Login";
+import Entry  from "./pages/Entry";
 
 const App = () => {
   return (
     <div>
-      App
+      <ToastContainer />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/Entry" element={<Entry />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
