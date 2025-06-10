7b6e6225d51255f7e3966b93f8bd32a6f00925b9
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-09 21:17:56 -0700

    login form making

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  const [state,setState] = useState('SignUp')
   return (
-    <div></div>
+    <div className='flex justify-center items-center h-screen '>
+      <form >
+        <h1>{state}</h1>
+        <input type='text'/>
+        <input type='text'/>
+        <input type='text'/>
+        <button type='submit'>{state}</button>
+      </form>
+    </div>
   )
 }
 
