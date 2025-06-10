a611ef37df28a9230b1a743afa482757ba1a57b6
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-06-09 21:13:21 -0700

    navbar completed

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div></div>
-  )
-}
+    <div className="bg-gray-400 p-4 text-white">
+      <div className="max-w-7xl mx-auto flex justify-between">
+        <h1 className="font-bold text-2xl">EntryAccountSystem</h1>
+        <button className="font-mono text-lg border rounded-full py-2 px-4 bg-gray-500 hover:bg-gray-600 cursor-pointer">Logout</button>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
